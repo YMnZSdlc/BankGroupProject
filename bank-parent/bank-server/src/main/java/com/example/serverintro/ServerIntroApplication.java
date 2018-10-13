76b407d1a1a49f1bc9f249f694e499c7568cf8b4
Mariusz Andrzejewski <mariusz.andrzejewski85@gmail.com>
2018-10-13 13:42:44 +0200

    Połączenie pom.xml ze springinitialzr ze modułem bank-server

---
+++ bank-parent/bank-server/src/main/java/com/example/serverintro/ServerIntroApplication.java
@@ -0,0 +1,12 @@
+package com.example.serverintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerIntroApplication.class, args);
+	}
+}

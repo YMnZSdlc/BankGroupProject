77bb10947d7981922cd3e89b499517890df2221d
Darek Fornalczyk <dariusz.fornalczyk@gmail.com>
2018-10-14 11:58:05 +0200

    Class sutomer, class AccountHistory, Enum Sex

--- bank-parent/bank-server/src/main/java/com/example/serverintro/Customer.java
+++ bank-parent/bank-server/src/main/java/com/example/serverintro/Customer.java
@@ -1,4 +1,85 @@
 package com.example.serverintro;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String pesel;
+    private Date createDate;
+    private String email;
+    private int phoneNo;
+    private Date dateOfBirth;
+    private Sex sex;
+
+
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
--- bank-parent/bank-server/src/main/java/com/example/serverintro/Sex.java
+++ bank-parent/bank-server/src/main/java/com/example/serverintro/Sex.java
@@ -1,5 +1,5 @@
 package com.example.serverintro;
 
-public enum CustomerSex {
+public enum Sex {
 man,woman
 }

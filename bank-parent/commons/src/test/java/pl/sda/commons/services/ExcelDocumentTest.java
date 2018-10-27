c69f8977cbaf5cc77ee9988ef6042afdbb60a8bd
YMnZSdlc <michal.mielczarek@gmail.com>
2018-10-27 13:01:15 +0200

    MM

--- bank-parent/commons/src/test/java/pl/sda/commons/services/ExcelDocumentTest.java
+++ bank-parent/commons/src/test/java/pl/sda/commons/services/ExcelDocumentTest.java
@@ -12,44 +12,98 @@ class ExcelDocumentTest {
     @Test
     void shouldSaveMockDataToXlsFile() {
         //given
-        List<String> exampleList = new ArrayList<String>();
+        List<String> exampleList = new ArrayList<>();
         exampleList.add("Sample 1");
         exampleList.add("Sample 2");
         exampleList.add("Sample 3");
         exampleList.add("Sample 4");
 
-        MockData exampleToWtite = new MockData(22, "Hello ", "World!!", exampleList);
+        MockData exampleToWrite = new MockData(22, "Hello ", "World!!", exampleList);
 
         //when
         ExcelDocument document = new ExcelDocument();
-        Boolean actualResult = document.saveOneToXLS(exampleToWtite);
+        Boolean actualResult = document.saveOneToXLS(exampleToWrite);
 
         //then
         Assertions.assertTrue(actualResult);
     }
 
     @Test
-    void shouldSaveMockDataListToXlsFile() {
+    void shouldGenerateMockDataToXlsFile() {
         //given
-        List<String> exampleList = new ArrayList<String>();
+        List<String> exampleList = new ArrayList<>();
         exampleList.add("Sample 1");
         exampleList.add("Sample 2");
         exampleList.add("Sample 3");
         exampleList.add("Sample 4");
 
-        List<MockData> exampleListToWtite = new ArrayList<>();
-        exampleListToWtite.add(new MockData(1, "AAA", "aaa", exampleList));
-        exampleListToWtite.add(new MockData(2, "BBB", "bbb", exampleList));
-        exampleListToWtite.add(new MockData(3, "CCC", "ccc", exampleList));
-        exampleListToWtite.add(new MockData(4, "DDD", "ddd", exampleList));
+        MockData exampleToWrite = new MockData(22, "Hello ", "World!!", exampleList);
+
+        //when
+        ExcelDocument document = new ExcelDocument();
+        Boolean actualResult = document.generateXLS(exampleToWrite);
+
+        //then
+        Assertions.assertTrue(actualResult);
+    }
+
+    @Test
+    void shouldSaveListMockDataToXlsFile() {
+        //given
+        List<String> exampleList = new ArrayList<>();
+        exampleList.add("Sample 1");
+        exampleList.add("Sample 2");
+        exampleList.add("Sample 3");
+        exampleList.add("Sample 4");
+
+        List<MockData> exampleListToWrite = new ArrayList<>();
+        exampleListToWrite.add(new MockData(1, "AAA", "aaa", exampleList));
+        exampleListToWrite.add(new MockData(2, "BBB", "bbb", exampleList));
+        exampleListToWrite.add(new MockData(3, "CCC", "ccc", exampleList));
+        exampleListToWrite.add(new MockData(4, "DDD", "ddd", exampleList));
+
+        //when
+        ExcelDocument document = new ExcelDocument();
+        Boolean actualResult = document.saveListToXLS(exampleListToWrite);
+
+        //then
+        Assertions.assertTrue(actualResult);
+    }
+
+    @Test
+    void shouldGenerateListMockDataToXlsFile() {
+        //given
+        List<String> exampleList = new ArrayList<>();
+        exampleList.add("Sample 1");
+        exampleList.add("Sample 2");
+        exampleList.add("Sample 3");
+        exampleList.add("Sample 4");
+
+        List<MockData> exampleListToWrite = new ArrayList<>();
+        exampleListToWrite.add(new MockData(1, "AAA", "aaa", exampleList));
+        exampleListToWrite.add(new MockData(2, "BBB", "bbb", exampleList));
+        exampleListToWrite.add(new MockData(3, "CCC", "ccc", exampleList));
+        exampleListToWrite.add(new MockData(4, "DDD", "ddd", exampleList));
 
 
         //when
         ExcelDocument document = new ExcelDocument();
-        Boolean actualResult = document.saveListToXLS(exampleListToWtite);
+        Boolean actualResult = document.generateXLS(exampleListToWrite);
 
         //then
         Assertions.assertTrue(actualResult);
+    }
+
+    @Test
+    void shouldReturnFalseToNull() {
+        //given
+        MockData exampleToWrite = null;
+
+        //when
+        ExcelDocument document = new ExcelDocument();
+        Boolean actualResult = document.generateXLS(exampleToWrite);
+
+        //then
 
     }
 

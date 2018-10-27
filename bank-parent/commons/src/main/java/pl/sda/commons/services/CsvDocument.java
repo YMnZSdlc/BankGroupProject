3d81a6e06a561eac0fdbf891a4dd3419b1465bb6
Mateusz <mateusz.olewinski96@gmail.com>
2018-10-27 11:08:50 +0200

    Generic Csv

--- bank-parent/commons/src/main/java/pl/sda/commons/services/CsvDocument.java
+++ bank-parent/commons/src/main/java/pl/sda/commons/services/CsvDocument.java
@@ -11,7 +11,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,57 +18,72 @@ public class CsvDocument {
 
     private static final String SAMPLE_CSV_FILE = "./sample.csv";
 
-    public static void main(String[] args) throws IOException {
-        printToFile(mockDataList());
+    //
+//    public static void main(String[] args) throws IOException {
+
+//        printToFile(prepareObject());
+//        printToFile(prepareList());
+//        printToFile(null);
+
+//    }
+
+    private static void CsvGenerator(Object data) throws IOException {
+        assert data != null : "Null Object";
+        printToFile(data);
     }
 
-    private static boolean printToFile(List<MockData> mockDataList) throws IOException {
+
+    private static void printToFile(Object data) throws IOException {
         BufferedWriter writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE));
-        MockData mockData = mockDataList.get(0);
-        Field[] fields = mockData.getClass().getDeclaredFields();
-        List<String> fieldNames = Arrays.stream(fields).map(Field::getName).collect(Collectors.toList());
 
-        String[] headers = new String[fieldNames.size()];
-        headers = fieldNames.toArray(headers);
-
+        String[] headers = extractHeaders(data);
 
         try (CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader(headers))) {
-            for (MockData data : mockDataList) {
-                List<Object> collect = Arrays.stream(data.getClass().getDeclaredFields()).map(field -> {
-                    try {
-                        field.setAccessible(true);
-                        return field.get(data);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                }).collect(Collectors.toList());
-                printer.printRecord(collect);
-            }
+            List<Object> collect = Arrays.stream(data.getClass().getDeclaredFields())
+                    .map(field -> setPermissionToFieldAccess(data, field))
+                    .collect(Collectors.toList());
+
+            printer.printRecord(collect);
+
         }
-        return true;
     }
 
-    static boolean saveToCSV(List<MockData> mockData) throws IOException {
-        if (!mockData.getClass().isAssignableFrom(Collection.class)) {
-            return printToFile(mockData);
+    private static Object setPermissionToFieldAccess(Object data, Field field) {
+        try {
+            field.setAccessible(true);
+            return field.get(data);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
-        return false;
+        return null;
     }
 
-    private static List<MockData> mockDataList() {
-        List<String> exampleList = new ArrayList<>();
-        exampleList.add("Sample 1");
-        exampleList.add("Sample 2");
-        exampleList.add("Sample 3");
-        exampleList.add("Sample 4");
-        List<MockData> mockData = new ArrayList<>();
-        mockData.add(new MockData(1, "asfga", "asf", exampleList));
-        mockData.add(new MockData(2, "asfga", "asf", exampleList));
-        mockData.add(new MockData(3, "asfga", "asf", exampleList));
-        mockData.add(new MockData(4, "asfga", "asf", exampleList));
-        mockData.add(new MockData(5, "asfga", "asf", exampleList));
-        return mockData;
-    }
+    private static String[] extractHeaders(Object data) {
 
+        Field[] fields = data.getClass().getDeclaredFields();
+        List<String> fieldNames = Arrays.stream(fields).map(Field::getName).collect(Collectors.toList());
+        String[] headers = new String[fieldNames.size()];
+        headers = fieldNames.toArray(headers);
+        return headers;
+    }
+//
+//    private static List<MockData> prepareList() {
+//        List<String> exampleList = new ArrayList<>();
+//        exampleList.add("Sample 1");
+//        exampleList.add("Sample 2");
+//        exampleList.add("Sample 3");
+//        exampleList.add("Sample 4");
+//
+//        List<MockData> mockData = new ArrayList<>();
+//        mockData.add(new MockData(1, "asfga", "asf", exampleList));
+//        mockData.add(new MockData(2, "asfga", "asf", exampleList));
+//        mockData.add(new MockData(3, "asfga", "asf", exampleList));
+//        mockData.add(new MockData(4, "asfga", "asf", exampleList));
+//
+//        return mockData;
+//    }
+//
+//    private static MockData prepareObject() {
+//        return new MockData(1, "asfga", "asf", null);
+//    }
 }

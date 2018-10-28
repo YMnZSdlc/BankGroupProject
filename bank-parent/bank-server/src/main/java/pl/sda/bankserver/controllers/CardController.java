004d14cac8b94e5fa220e6fa80847e6ddae3032d
Adam Konieczny <az_k@gazeta.pl>
2018-10-28 15:08:46 +0100

    added some more

--- bank-parent/bank-server/src/main/java/pl/sda/bankserver/controllers/CardController.java
+++ bank-parent/bank-server/src/main/java/pl/sda/bankserver/controllers/CardController.java
@@ -2,15 +2,29 @@ package pl.sda.bankserver.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import pl.sda.bankcommons.domain.dtos.CardRegistrationDto;
+import pl.sda.bankserver.domain.Card;
+
+import java.util.List;
 
 @RestController
 public class CardController {
 
 
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public void createCard(@RequestBody) {
-//
-//    }
+    @PostMapping("/server/card/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createCard(@RequestBody CardRegistrationDto registrationDto) {
+
+    }
+
+    @GetMapping("/server/card/all")
+    public List<Card> findAll() {
+        return null;
+    }
+
+    @GetMapping("/server/card/findbyid")
+    public Card findCardById() {
+        return null;
+    }
 
 }

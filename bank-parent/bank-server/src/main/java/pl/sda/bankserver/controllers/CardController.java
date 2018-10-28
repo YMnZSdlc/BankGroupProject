6253d1d9dbe6a6269851af5e0630cc03ea2c0c2b
Adam Konieczny <az_k@gazeta.pl>
2018-10-28 14:32:46 +0100

    Added classes

--- bank-parent/bank-server/src/main/java/pl/sda/bankserver/controllers/CardController.java
+++ bank-parent/bank-server/src/main/java/pl/sda/bankserver/controllers/CardController.java
@@ -1,7 +1,16 @@
 package pl.sda.bankserver.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CardController {
+
+
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public void createCard(@RequestBody) {
+//
+//    }
+
 }

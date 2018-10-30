7d14c64ef3e1e3e1554e0597338a56edf51073e0
Jacob <dobrogowskijakub@gmail.com>
2018-10-30 19:44:16 +0100

    properties configuration

--- bank-parent/bank-server/src/main/java/pl/sda/bankserver/config/LoadProperties.java
+++ bank-parent/bank-server/src/main/java/pl/sda/bankserver/config/LoadProperties.java
@@ -6,23 +6,14 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 
-import javax.annotation.PostConstruct;
-
 @Configuration
 @PropertySources({
-        @PropertySource("classpath:application.properties"),
-        @PropertySource("file:${user.home}/newProperties.properties")
+        @PropertySource("classpath:generator.properties"),
+        @PropertySource(value = "file:${user.home}/application.properties", ignoreResourceNotFound = true)
 })
 public class LoadProperties {
 
     @Autowired
     Environment env;
 
-
-    @PostConstruct
-    public void log() {
-
-        String dupa = env.getProperty("dupa");
-        System.out.println(dupa);
-    }
 }

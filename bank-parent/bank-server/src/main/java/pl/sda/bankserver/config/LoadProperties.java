8d9b390aaec8ae4316aaeeb8f23d1bcf2ebdfe91
marcinerbel <marcinerbel90@gmail.com>
2018-11-02 22:22:09 +0100

    Little Clean Up

--- bank-parent/bank-server/src/main/java/pl/sda/bankserver/config/LoadProperties.java
+++ bank-parent/bank-server/src/main/java/pl/sda/bankserver/config/LoadProperties.java
@@ -1,10 +1,8 @@
 package pl.sda.bankserver.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySources({

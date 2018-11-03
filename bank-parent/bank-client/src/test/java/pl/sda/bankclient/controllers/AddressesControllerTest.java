6bf393d5d8c80c813f64cd59f2ca1a5058d8d7e4
Logan <logan09557@gmail.com>
2018-11-03 13:41:23 +0100

    testy poprawione

--- bank-parent/bank-client/src/test/java/pl/sda/bankclient/controllers/AddressesControllerTest.java
+++ bank-parent/bank-client/src/test/java/pl/sda/bankclient/controllers/AddressesControllerTest.java
@@ -1,19 +1,25 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+package pl.sda.bankclient.controllers;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(Arquillian.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class AddressesControllerTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(pl.sda.bankclient.controllers.AddressesController.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnOkStatus() throws Exception{
+        this.mockMvc.perform(get("/addresses/save")).andDo(print()).andExpect(status().isOk());
     }
 
 }

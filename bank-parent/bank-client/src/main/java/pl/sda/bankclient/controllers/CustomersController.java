19c406a744190d3c87f21ecec7e4d565631b65ea
sda_ldz_9_3 <sda_ldz_9_3@wp.pl>
2018-11-03 11:47:51 +0000

    Thymeleaf

---
+++ bank-parent/bank-client/src/main/java/pl/sda/bankclient/controllers/CustomersController.java
@@ -0,0 +1,71 @@
+package pl.sda.bankclient.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.bankcommons.domain.dtos.CustomerRegistrationDto;
+
+@Controller
+public class CustomersController {
+
+    @GetMapping("/customers")
+    public String showCustomers() {
+        return "customers";
+    }
+
+    @GetMapping("/customers/save")
+    public String viewFormToCreateCustomer(Model model) {
+        model.addAttribute("save", new CustomerRegistrationDto());
+        return "customers/save";
+    }
+
+    @PostMapping("/customers/save")
+    public String saveCustomer(@ModelAttribute CustomerRegistrationDto customerDto) {
+        return "customers/save";
+    }
+
+    @GetMapping("/customers/find")
+    public String viewFormToFindCustomer(Model model) {
+        model.addAttribute("find", new CustomerRegistrationDto());
+        return "customers/find";
+    }
+
+    @PostMapping("/customers/find")
+    public String findCustomer(@ModelAttribute CustomerRegistrationDto customerDto) {
+        return "customers/find";
+    }
+
+    @GetMapping("/customers/findall")
+    public String findAllCustomersByParam(Model model) {
+        model.addAttribute("findall", new CustomerRegistrationDto());
+        return "customers/findall";
+    }
+
+    @PostMapping("/customers/findall")
+    public String findAllCustomers(@ModelAttribute CustomerRegistrationDto customerDto) {
+        return "customers/findall";
+    }
+
+    @GetMapping("/customers/update")
+    public String chooseCustomerForUpdate(Model model) {
+        model.addAttribute("update", new CustomerRegistrationDto());
+        return "customers/update";
+    }
+
+    @PutMapping("/customers/update")
+    public String updateCustomer(@ModelAttribute CustomerRegistrationDto customerDto) {
+        return "customers/update";
+    }
+
+    @GetMapping("customers/delete")
+    public String findCustomerToDelete(Model model) {
+        model.addAttribute("delete", new CustomerRegistrationDto());
+        return "customers/delete";
+    }
+
+    @DeleteMapping("customers/delete")
+    public String deleteCustomer(@ModelAttribute CustomerRegistrationDto customerDto) {
+        return "customers/delete";
+    }
+}
---
+++ bank-parent/bank-client/src/main/java/pl/sda/bankclient/controllers/ServicesController.java
@@ -0,0 +1,70 @@
+package pl.sda.bankclient.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.bankcommons.domain.dtos.ServiceCreationDto;
+
+@Controller
+public class ServicesController {
+
+    @GetMapping("/services")
+    public String showServices() {
+        return "services";
+    }
+
+    @GetMapping("/services/save")
+    public String viewFormToCreateService(Model model) {
+        model.addAttribute("save", new ServiceCreationDto());
+        return "services/save";
+    }
+
+    @PostMapping("/services/save")
+    public String saveService(@ModelAttribute ServiceCreationDto serviceCreationDto) {
+        return "services/save";
+    }
+
+    @GetMapping("/services/find")
+    public String viewFormToFindService(Model model) {
+        model.addAttribute("find", new ServiceCreationDto());
+        return "services/find";
+    }
+
+    @PostMapping("/services/find")
+    public String findService(@ModelAttribute ServiceCreationDto serviceCreationDto) {
+        return "services/find";
+    }
+
+    @GetMapping("/services/findall")
+    public String findAllServicesByParam(Model model) {
+        model.addAttribute("findall", new ServiceCreationDto());
+        return "services/findall";
+    }
+
+    @PostMapping("/services/findall")
+    public String findAllServices(@ModelAttribute ServiceCreationDto serviceCreationDto) {
+        return "services/findall";
+    }
+
+    @GetMapping("/services/update")
+    public String chooseServiceForUpdate(Model model) {
+        model.addAttribute("update", new ServiceCreationDto());
+        return "services/update";
+    }
+
+    @PutMapping("/services/update")
+    public String updateService(@ModelAttribute ServiceCreationDto serviceCreationDto) {
+        return "services/update";
+    }
+
+    @GetMapping("services/delete")
+    public String findServiceToDelete(Model model) {
+        model.addAttribute("delete", new ServiceCreationDto());
+        return "services/delete";
+    }
+
+    @DeleteMapping("services/delete")
+    public String deleteService(@ModelAttribute ServiceCreationDto serviceCreationDto) {
+        return "services/delete";
+    }
+}
